6995fd9b11b4758b2e7717f0ca58783e08b658f4
pritha-biswas-prama <oritaoribiswas@gmail.com>
2025-08-06 13:44:12 +0600

    Initial commit

---
+++ validation.js
@@ -0,0 +1,19 @@
+<scrpit>
+    function validation() {
+        var Name= document.getEleentById("name").value;
+        var Age= document.getEleentById("age").value;
+        var Phone= document.getEleentById("phone").value;
+        var emailmail adress= document.getEleentById("email").value;
+        var Insurence Provider= document.getEleentById("insurence").value;
+        var Insurence Policy Number= document.getEleentById("ipnumber").value;
+        var Username= document.getEleentById("username").value;
+        var Password= document.getEleentById("Password").value;
+        var ConfirmPass= document.getEleentById("confirmPass").value;
+        
+        if (full name=="") {
+            
+        }
+
+        
+    }
+</scrpit>
